cd62ce9c87e5508f7cf9b264a525f693096e85c2
robbedec <robbe.decorte@student.hogent.be>
2019-10-13 19:15:52 +0200

    fix: home fragment

--- app/src/main/java/com/example/android/calorietracker/MainActivity.kt
+++ app/src/main/java/com/example/android/calorietracker/MainActivity.kt
@@ -2,11 +2,17 @@ package com.example.android.calorietracker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.drawerlayout.widget.DrawerLayout
+import com.example.android.calorietracker.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    // private lateinit var drawerLayout: DrawerLayout
+    // private lateinit var appBarConfiguration: AppBarConfiguration
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
     }
 }
