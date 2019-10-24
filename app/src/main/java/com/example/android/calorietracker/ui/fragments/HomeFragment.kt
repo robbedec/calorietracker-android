a143ae4b2bcc89eaf5835c54f73d576a02430044
robbedec <robbe.decorte@student.hogent.be>
2019-10-24 12:58:17 +0200

    refactor: update calorie values with viewmodel

--- app/src/main/java/com/example/android/calorietracker/ui/fragments/HomeFragment.kt
+++ app/src/main/java/com/example/android/calorietracker/ui/fragments/HomeFragment.kt
@@ -7,15 +7,27 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProviders
 import com.example.android.calorietracker.R
 import com.example.android.calorietracker.databinding.FragmentHomeBinding
+import com.example.android.calorietracker.ui.viewModels.HomeViewModel
 
 class HomeFragment : Fragment() {
 
+    private lateinit var binding: FragmentHomeBinding
+    private lateinit var viewModel: HomeViewModel
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate xml in onCreateView
-        val binding: FragmentHomeBinding = DataBindingUtil.inflate(inflater,
+        binding = DataBindingUtil.inflate(inflater,
             R.layout.fragment_home, container, false)
+
+        // Request the ViewModal
+        viewModel = ViewModelProviders.of(this).get(HomeViewModel::class.java)
+        binding.homeViewModal = viewModel
+
+        binding.setLifecycleOwner(this)
+
         return binding.root
     }
 
---
+++ app/src/main/java/com/example/android/calorietracker/ui/viewModels/HomeViewModel.kt
@@ -0,0 +1,46 @@
+package com.example.android.calorietracker.ui.viewModels
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import timber.log.Timber
+
+class HomeViewModel : ViewModel() {
+
+    /*
+     * The current amount of calories
+     */
+    private val _currentCalories = MutableLiveData<Int>()
+    val currentCalories: LiveData<Int>
+        get() = _currentCalories
+
+    /*
+     * The maximum amount of calories (the goal that the user wants to reach
+     */
+    val goal: MutableLiveData<Int>
+
+    /*
+     * The ratio between current amount of calories and the maximum amount
+     */
+    private val _percentage = MutableLiveData<Int>()
+    val percentage: LiveData<Int>
+        get() = _percentage
+
+
+    init {
+        Timber.i("HomeViewModel created")
+        _currentCalories.value = 250
+        goal = MutableLiveData(500)
+
+        calcPercentage()
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        Timber.i("GameViewModel destroyed")
+    }
+
+    private fun calcPercentage() {
+        _percentage.value = (_currentCalories.value!! * 100.0f / goal.value!!).toInt()
+    }
+}
